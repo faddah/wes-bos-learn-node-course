b8565a0b3c3c08d883db001c42419ef07bfb7c1a
Faddah Wolf <my_biz@me.com>
2022-12-23 07:20:17 -0800

    Per A-J Roos, made them old school ES5 functions.

--- starter-files/public/javascripts/modules/typeAhead.js
+++ starter-files/public/javascripts/modules/typeAhead.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { $ } from "./bling";
 
-const typeAhead = search => {
+function typeAhead(search) {
 	if(!search) return; 
 	const searchInput = search.querySelector('input[name="search"]');
 	const searchResults = search.querySelector('.search__results');
 	// console.log(search, searchInput, searchResults);
-	searchInput.on('input', () => {
+	searchInput.on('input', function() {
 		// if there is no value, just quit it already.
 		if(!this.value) {
 			searchResults.style.display = 'none';
