8b2e90f44c8008bc365ba2319f9e9bad723c1cd8
Faddah Wolf <my_biz@me.com>
2022-12-23 10:17:07 -0800

    Commit & pushing again for more typeAhead issues.

--- starter-files/public/javascripts/delicious-app.js
+++ starter-files/public/javascripts/delicious-app.js
@@ -2,8 +2,8 @@ import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
-import makeMap from "./modules/map";
 import typeAhead from "./modules/typeAhead";
+import makeMap from "./modules/map";
 
 autocomplete( $('#address'), $('#lat'), $('#lng') );
 
--- starter-files/public/javascripts/modules/typeAhead.js
+++ starter-files/public/javascripts/modules/typeAhead.js
@@ -1,5 +1,17 @@
-import axios from "axios";
-import { $ } from "./bling";
+import axios from 'axios';
+import dompurify from 'dompurify';
+
+function searchResultsHTML(stores) {
+	return stores.map(store => {
+		return `
+			<p style="font-size: 1.5em;">
+				<a href="/store/${store.slug}" className="search__result">
+					<strong>${store.name}</strong>
+				</a>
+			</p>
+		`;
+		}).join('');
+}
 
 function typeAhead(search) {
 	if(!search) return; 
@@ -12,18 +24,48 @@ function typeAhead(search) {
 			searchResults.style.display = 'none';
 			return; // stop!
 		}
-		// console.log(this.value);
 		// show the search results div
 		searchResults.style.display = 'block';
+		searchResults.innerHTML = '';
 		axios
-			.get(`api/search/q?={this.value}`)
-			.then(res => {
-				console.log(this.value);
-				// console.log(res.data);
-				}
-			)
-	});
-}
+		.get(`/api/search?q=${this.value}`)
+		.then(res => {
+			if(res.data.length) {
+				searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+				return;
+			}
+			// tell user nothing came back from search
+			searchResults.innerHTML = dompurify.sanitize(`<div className="search__result">No search results for ${this.value}.</div>`);
+		})
+		.catch(err => {
+			console.error(`You have an error on search: ${err}.`);
+		});
+});
 
+	// handle keyboard inputs
+	searchInput.on('keyup', (e) => {
+		if(![38, 40, 13].includes(e.keyCode)) {
+			return;  // nah, brah.
+		}
+		const activeClass = 'search__result--active'; 
+		const current = search.querySelector(`.${activeClass}`);
+		const items = search.querySelector(`.search__result`);
+		let next;
+		if(e.keyCode === 40 && current) {
+			next = current.nextElementSibling || items[0];
+		} else if(e.keyCode === 40) {
+			next = items[0];
+		} else if (e.keyCode === 38 && current) {
+			next = current.previousElementSibling || items[items.length - 1];
+		} else if (e.keyCode === 38) {
+			next = items[items.length - 1];
+		} else if (e.keyCode === 13 && current.href) {
+			window.location = current.href;
+			return;
+		}
+		if(current) current.classList.remove(activeClass);
+		next.classList.add(activeClass);
+	})
+}
 
 export default typeAhead;
