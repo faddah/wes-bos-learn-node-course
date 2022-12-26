861bb80b5f8d1a1fdfaafedb187c283356840610
Faddah Wolf <my_biz@me.com>
2022-12-26 11:34:58 -0800

    Commit & push for issues seen after video #34.

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -139,7 +139,7 @@ exports.mapStores = async (req, res) => {
 		}
 	}
 	const stores = await Store.find(query)
-		.select('slug name description location')
+		.select('slug name description location photo')
 		.limit(10);
 	res.json(stores);
 }
--- starter-files/public/javascripts/modules/map.js
+++ starter-files/public/javascripts/modules/map.js
@@ -1,58 +1,69 @@
 import axios from 'axios';
-import { addListener } from '../../../models/Store';
-import { $ } from "./bling";
+import { $ } from './bling';
 
 const mapOptions = {
 	center: { lat: 43.2, lng: -79.8 },
 	zoom: 10
-}
-	
+};
+
 function loadPlaces(map, lat = 43.2, lng = -79.8) {
-	axios
-	.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
-	.then(res => {
-		const places = res.data;
-		if (!places.length) {
-			console.log('There were no places nearby found for that location.');
-			alert('No places found!');
-			return;
-		}
-		// create a bounds
-		const bounds = new google.maps.LatLngBounds();
-		const infoWindow = new google.mapsInfoWindow();
+	axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`)
+		.then(res => {
+			const places = res.data;
+			if (!places.length) {
+				console.log(`There were no nearby locations found for that search.`);
+				alert('no places found!');
+				return;
+			}
+			// create a bounds
+			const bounds = new google.maps.LatLngBounds();
+			const infoWindow = new google.maps.InfoWindow();
 
-		const markers = places.map(place => {
-			const [placeLng, placeLat] = place.location.coordinates;
-			const position = { lat: placeLat, lng: placeLng };
-			bounds.extend(position);
-			const marker = new google.maps.Marker({ map, position });
-			marker.place = place;
-			return marker;
+			const markers = places.map(place => {
+				const [placeLng, placeLat] = place.location.coordinates;
+				const position = { lat: placeLat, lng: placeLng };
+				bounds.extend(position);
+				const marker = new google.maps.Marker({ map, position });
+				marker.place = place;
+				return marker;
+			});
+
+			// when someone clicks on a marker, show the details of that place
+			markers.forEach(marker => marker.addListener('click', function() {
+				// console.log(this.place);
+				const html = `
+					<div className="popup">
+						<a href="/store/${this.place.slug}>
+							<img src="/uploads/${this.place.photo || 'store.png'}" alt="${this.place.name}" />
+							<p>${this.place.name} — ${this.place.location.address}</p>
+						</a>		
+					</div>
+				`
+				infoWindow.setContent(html);
+				infoWindow.open(map, this);
+			}));
+
+			// then zoom the map to fit all the markers perfectly
+			map.setCenter(bounds.getCenter());
+			map.fitBounds(bounds);
 		});
-
-		// when someone clicks on a marker, show the details of that place
-		markers.forEach(marker => marker.addListener('click', function() {
-			// console.log(this.place);
-			infoWindow.setContent(this.place.name)
-			infoWindow.open(map, this);
-		}));
-
-		// then zoom the map to fit all the markers perfectly
-		map.setCenter(bounds.getCenter());
-		map.fitBounds(bounds);
-	});
 
 }
 
 function makeMap(mapDiv) {
-	if(!mapDiv) return;
-	// let us now make our map
+	if (!mapDiv) return;
+	// make our map
 	const map = new google.maps.Map(mapDiv, mapOptions);
 	loadPlaces(map);
 
 	const input = $('[name="geolocate"]');
-	console.log(input);
 	const autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.addListener('place_changed', () => {
+		const place = autocomplete.getPlace();
+		// console.log(place);
+		const { lat, lng } = place.geometry.location;
+		loadPlaces(map, lat(), lng());
+	})
 }
 
 export default makeMap;
